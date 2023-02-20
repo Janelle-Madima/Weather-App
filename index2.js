d6da133a56041d52fe8ea9b2200ade0e9f8a469a
Janelle-Madima <janemadima@gmail.com>
2023-02-20 13:37:21 +0200

    Added js

---
+++ index2.js
@@ -0,0 +1,94 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+// Feature #1
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+// Feature #2
+function searchCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = `${cityInput.value}`;
+}
+
+let cityForm = document.querySelector("#search-form");
+cityForm.addEventListener("submit", searchCity);
+
+// 🙀Bonus Feature
+// Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
+function toFahrenheit(event) {
+  let temperatureChange = document.querySelector("#temperature");
+  temperatureChange.innerHTML =
+    Math.round(temperatureChange.innerHTML * 9) / 5 + 32;
+}
+
+function toCelcius(event) {
+  let temperatureChange = document.querySelector("#temperature");
+  temperatureChange.innerHTML = Math.round(
+    ((temperatureChange.innerHTML - 32) * 5) / 9
+  );
+}
+let temperatureF = document.querySelector("#fahrenheit-link");
+temperatureF.addEventListener("click", toFahrenheit);
+let temperatureC = document.querySelector("#celsius-link");
+temperatureC.addEventListener("click", toCelcius);
+
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  // document.querySelector("#description").innerHTML =
+  //   response.data.weather[0].description;
+}
+function searchCurrentCity(city) {
+  let apiKey = "1480f129aeb5599f48231386eca5a274";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function searchPosition(position) {
+  let apiKey = "b213b826dff2076f54e8bec53e754177";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchPosition);
+}
+
+let currentLocation = document.querySelector("#current-button");
+
+currentLocation.addEventListener("click", getCurrentLocation);
